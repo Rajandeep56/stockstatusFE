5853b9fe1cfdcf2c792a25e2a6aa8d17bb5b79be
Rajandeep56 <rajandeepkaur61@outlook.com>
2024-03-24 15:24:49 -0700

    Updated Actions with temp auth token

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Stock from './pages/Stock/Stock';
+import StockDetails from './components/StockDetails';
+import EditStockDetails from './components/editStock';
 import './App.css';
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/stock" element={<Stock />} />
+        <Route path="/stock/:id" element={<StockDetails />} />
+        <Route path="/stock/:id/edit" element={<EditStockDetails />} />
       </Routes>
     </Router>
   );
