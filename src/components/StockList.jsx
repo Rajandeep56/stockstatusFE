5f591a02b6d8f7b38504e9a6131aa47a33079f7f
Rajandeep56 <rajandeepkaur61@outlook.com>
2024-03-24 13:51:39 -0700

    Updated Api Call with temp auth code

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,10 +1,28 @@
+import axios from 'axios';
 import React from 'react';
-
+import { useState, useEffect } from 'react';
 const StockList = () => {
+    const [stockList, setStockList] = useState([]);
+    //temp setup for auth
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImphbmUiLCJpYXQiOjE3MTEzMTMxNTR9.bY2mHQa-e_lltaGtfc_8Cgs9S2EM5yFy9_p4_6vUkIQ';
+    const auth = { headers: {Authorization: `Bearer ${token}`}}
+    useEffect(() => {
+        axios.get('http://localhost:8088/api/list',auth).then(response => {
+            setStockList(response.data);
+        }).catch(error => {
+            console.log(error);
+        });
+    }, []);
+
     return (
         <div>
             <h1>Stock List</h1>
-            <p>This is the stock list component.</p>
+            <ul>
+                {stockList.map(item => (
+                    <li key={item.id}>{item.brand} {item.price_per_unit} {item.expiration_date} {item.quantity}</li>
+                    
+                ))}
+            </ul>
         </div>
     );
 };
