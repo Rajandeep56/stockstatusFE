82b90a5da9fa1468bea06e5ec30f9cf8d2794b48
Rajandeep56 <rajandeepkaur61@outlook.com>
2024-03-24 13:28:55 -0700

    Stock List Component Setup

---
+++ src/components/StockList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StockList = () => {
+    return (
+        <div>
+            <h1>Stock List</h1>
+            <p>This is the stock list component.</p>
+        </div>
+    );
+};
+
+export default StockList;
---
+++ src/pages/Stock/Stock.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import StockList from '../../components/StockList';
+
+const Stock = () => {
+    return (
+        <div>
+            <h1>Stock Page</h1>
+            <StockList />
+        </div>
+    );
+};
+
+export default Stock;
